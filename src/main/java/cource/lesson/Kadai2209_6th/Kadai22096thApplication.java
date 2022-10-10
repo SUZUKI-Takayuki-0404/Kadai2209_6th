b447bff2928e6dfa2574623cebeb9bd105ba3216
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-10-10 18:31:28 +0900

    Set up blank project

---
+++ src/main/java/cource/lesson/Kadai2209_6th/Kadai22096thApplication.java
@@ -0,0 +1,13 @@
+package cource.lesson.Kadai2209_6th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadai22096thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadai22096thApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cource/lesson/Kadai2209_6th/Kadai22096thApplicationTests.java
@@ -0,0 +1,13 @@
+package cource.lesson.Kadai2209_6th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kadai22096thApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
