130cdca2b569f3888ddbe71e31c84c97bf4469fb
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-10-16 21:01:40 +0900

    update_findAllCode-method_Using_SQL

--- src/main/java/cource/lesson/Kadai2209_6th/AirportRepository.java
+++ src/main/java/cource/lesson/Kadai2209_6th/AirportRepository.java
@@ -2,8 +2,10 @@ package cource.lesson.Kadai2209_6th;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,14 +24,16 @@ public class AirportRepository {
 
     public List<String> findAllCode() {
 
-        //Airport code list (tentative, to be replaced by SQL later)
-        String[] airportCodes = {
-                "NRT", "HND", "KIX", "NGO", "JFK", "BOS", "ORD", "DFW",
-                "SFO", "LAX", "SAN", "YVR", "HNL", "LHR", "CDG", "FRA",
-                "DME", "PVG", "SHA", "HKG", "BJS", "DLC", "TSN", "CAN",
-                "ICN", "GMP", "PUS", "TPE", "TSA", "KHH", "SGN", "HAN",
-                "BKK", "KUL", "SIN", "CGK", "MNL", "DEL", "GUM", "SYD"};
+        String sql = "SELECT airportCode" + " FROM airports";
 
-        return List.of(airportCodes);
+        List<String> foundCodeList = new ArrayList<>();
+
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql);
+
+        while (rowSet.next()) {
+            foundCodeList.add(rowSet.getString("airportCode"));
+        }
+
+        return foundCodeList;
     }
 }
