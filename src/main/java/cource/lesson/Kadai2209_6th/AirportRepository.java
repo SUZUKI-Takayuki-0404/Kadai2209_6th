b9b40c1e037ff950d25c2651dd361b7c5b860bc3
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-10-16 11:18:35 +0900

    add_findAllCode-method

--- src/main/java/cource/lesson/Kadai2209_6th/AirportRepository.java
+++ src/main/java/cource/lesson/Kadai2209_6th/AirportRepository.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -18,5 +19,17 @@ public class AirportRepository {
 
         return jdbcTemplate.queryForMap(query, airportCode);
     }
-    
+
+    public List<String> findAllCode() {
+
+        //Airport code list (tentative, to be replaced by SQL later)
+        String[] airportCodes = {
+                "NRT", "HND", "KIX", "NGO", "JFK", "BOS", "ORD", "DFW",
+                "SFO", "LAX", "SAN", "YVR", "HNL", "LHR", "CDG", "FRA",
+                "DME", "PVG", "SHA", "HKG", "BJS", "DLC", "TSN", "CAN",
+                "ICN", "GMP", "PUS", "TPE", "TSA", "KHH", "SGN", "HAN",
+                "BKK", "KUL", "SIN", "CGK", "MNL", "DEL", "GUM", "SYD"};
+
+        return List.of(airportCodes);
+    }
 }
